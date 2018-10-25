21f128fdf9a444dc2f6baa1441f96f3f6f08caa4
FaisholMuzaky <fmuzaki@gmail.com>
2018-10-25 12:42:04 +0700

    halalala

---
+++ controller.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// ini buat render file hbs
+app.set('view engine', 'hbs');
+
+// ini buat change default folder views
+app.use(express.static(path.join(__dirname, 'views')));
+// app.set("views", path.join(__dirname, "views"));
+
+//pages
+app.get('/', (req, res) => {
+   res.render('Home/home')
+})
+
+app.get('/about', (req, res) => {
+   res.render('About/about')
+})
+
+app.get('/contact', (req, res) => {
+   res.render('Contact/contact')
+})
+
+app.get('/menu', (req, res) => {
+   res.render('Menu/menu')
+})
+
+ app.get('/reservation', (req, res) => {
+    res.render('Reservation/Reservation')
+ })
+
+app.get('*', (req, res) => {
+  res.send('Page tidak ketemu')
+})
+  
+app.listen(process.env.PORT || 8000, () => {
+  console.log('8000')
+})
